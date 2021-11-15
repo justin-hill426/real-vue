8c4406966b7dea1c4c450d774e3a5dbde769ed06
justin-hill426 <jrhill426@gmail.com>
2021-11-15 08:35:51 -0700

    Added Mock API Server for EventList

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link
     class="event-link"
-    :to="{ name: 'event-show', params: { id: '1' } }"
+    :to="{ name: 'event-show', params: { id: event.id } }"
   >
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
@@ -14,21 +14,10 @@
 
 <script>
 export default {
-  data() {
-    return {
-      event: {
-        id: 1,
-        title: "Beach Cleanup",
-        date: "Tues Aug 19, 2018",
-        time: "6:00",
-        attendees: [
-          { id: "abc123", name: "Adam Jahr" },
-          { id: "abc456", name: "Justin Hill" },
-        ],
-      },
-    };
+  props: {
+    event: Object,
   },
-};
+}
 </script>
 
 <style scoped>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,18 +1,36 @@
 <template>
   <div>
     <h1>Events Listing: View Here</h1>
-    <EventCard></EventCard>
-    <BaseIcon/>
+    <EventCard
+      v-for="event in events"
+      :key="event.id"
+      :event="event"
+    ></EventCard>
+    <BaseIcon />
   </div>
-  
 </template>
 
 <script>
-import EventCard from "@/components/EventCard.vue";
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
 
 export default {
   components: {
-    EventCard
-  }
+    EventCard,
+  },
+  created() {
+    EventService.getEvents() // Does a get request
+      .then((response) => {
+        this.events = response.data
+      })
+      .catch((error) => {
+        console.log('There was an error:', error.response) // Logs out the error
+      })
+  },
+  data() {
+    return {
+      events: [],
+    }
+  },
 }
 </script>
